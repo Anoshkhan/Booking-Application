e92f2cff13da29652da8ae0b40adb5a70b02f024
Anosh Khan <anoshkhanakb123@gmail.com>
2022-09-05 09:51:32 +0500

    nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./navbar.css";
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+    <div className="navbar">
+        <div className="navContainer">
+        <span className="logo">Booking.com</span>
+        <div className="navItems">
+        <button className="navButton">Register</button>
+        <button className="navButton">Log in</button>
+        </div>
+        </div>
+    </div>
     );
   }
   
