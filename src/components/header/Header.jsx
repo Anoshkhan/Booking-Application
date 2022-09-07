bcf08f76ae29ec82eaa5769cd119c7e22dc51785
Anosh Khan <anoshkhanakb123@gmail.com>
2022-09-07 11:03:00 +0500

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import "./header.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBed,faPlane, faCar , faTaxi} from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
     return (
     <div className="header">
-    
+      <div className="headerContainer">
+        <div className="headerList">
+            <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+            </div>
+          </div>
+          <h1 className="headerTitle">
+            Find your next stay
+          </h1>
+          <p className="headerDesc">Get rewarded for your travels unlock. Search low prices for hotels, homes and much more.</p>
+          <button className="headerBtn">Sign in / Register </button>
+      </div>  
     </div>
     );
   }
